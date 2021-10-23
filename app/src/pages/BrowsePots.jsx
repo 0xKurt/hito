60facf58578d0d66e9ce306b8ecacaac230c3b21
Lilith <valerie.vaske@gmail.com>
2021-10-23 14:43:04 +0200

    Added table to BrowsePot's page

--- app/src/pages/BrowsePots.jsx
+++ app/src/pages/BrowsePots.jsx
@@ -1,19 +1,52 @@
 import React from 'react';
-import { Button, Card, Container, ListGroup } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import { useHistory } from 'react-router';
 import { useConnectedWeb3, useReadState, useWriteState } from '../web3/hooks';
 
 const BrowsePots = () => {
-  const history = useHistory();
+    const history = useHistory();
 
-  const onClickHandler = () => {
-    history.push('/exchange')
-  }
-  return (
-    <div className='body'>
-      Here you will be able to see all the existing pots. 
-    </div>
-  );
+    const onClickHandler = () => {
+        history.push('/exchange')
+    }
+    return (
+        <div className='body'>
+
+            <Table striped bordered hover >
+                <thead>
+                    <tr>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            Description
+                        </th>
+                        <th>
+                            Active
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Projekt 1</td>
+                        <td>Hier passieren Dinge</td>
+                        <td>Aktiv</td>
+                    </tr>
+                    <tr>
+                        <td>Projekt 2</td>
+                        <td>Hier passieren Dinge</td>
+                        <td>Inaktiv</td>
+                    </tr>
+                    <tr>
+                        <td>Projekt 3</td>
+                        <td>Hier passieren Dinge</td>
+                        <td>Aktiv</td>
+                    </tr>
+                </tbody>
+
+            </Table>
+        </div>
+    );
 }
 
 export default BrowsePots;
