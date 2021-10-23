373810d4ecd3ca5219a846199308eeb2f3db6c72
Lilith <valerie.vaske@gmail.com>
2021-10-23 13:46:40 +0200

    All form fields existing
    Ugly. Works on my machine. No logic, no cables.

--- app/src/pages/CreatePot.jsx
+++ app/src/pages/CreatePot.jsx
@@ -42,9 +42,103 @@ const CreatePot = () => {
           </Col>
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        <Form.Group as={Row} className="mb-3" controllId="formIDRange">
+          <Form.Label column sm={2} >NFT ID's to sell</Form.Label>
+          <Col sm={5}>
+            <Form.Control type="number" placeholder="From first ID..."></Form.Control>
+          </Col>
+          <Col sm={5}>
+            <Form.Control type="number" placeholder="...to last ID"></Form.Control>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controllId="formMaxUserBuy">
+          <Form.Label column sm={2} >Max Amount User can buy</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="number" placeholder="Amount"></Form.Control>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controllId="formNFTPrice">
+          <Form.Label column sm={2} >Price per NFT</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="number" placeholder="Price of NFT"></Form.Control>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controllId="formSaleStart">
+          <Form.Label column sm={2} >Sale Start Date</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="date" placeholder="Sale Start"></Form.Control>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controllId="formVoteStart">
+          <Form.Label column sm={2} >Vote Start Date</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="date" placeholder="Vote Start"></Form.Control>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controllId="formVoteStart">
+          <Form.Label column sm={2} >Vote Start Date</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="date" placeholder="Vote Start"></Form.Control>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controllId="formMilestone1">
+          <Form.Label column sm={2} >Milestone 1</Form.Label>
+          <Col sm={3}>
+            <Form.Control type="text" placeholder="Milestone name"></Form.Control>
+          </Col>
+          <Col sm={3}>
+            <Form.Control type="textarea" placeholder="Brief description"></Form.Control>
+          </Col>
+          <Col sm={2}>
+            <Form.Control type="decimal" placeholder="ETH"></Form.Control>
+          </Col>
+          <Col sm={2}>
+            <Form.Control type="date" placeholder="reached"></Form.Control>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controllId="formMilestone2">
+          <Form.Label column sm={2} >Milestone 2</Form.Label>
+          <Col sm={3}>
+            <Form.Control type="text" placeholder="Milestone name"></Form.Control>
+          </Col>
+          <Col sm={3}>
+            <Form.Control type="textarea" placeholder="Brief description"></Form.Control>
+          </Col>
+          <Col sm={2}>
+            <Form.Control type="decimal" placeholder="ETH"></Form.Control>
+          </Col>
+          <Col sm={2}>
+            <Form.Control type="date" placeholder="reached"></Form.Control>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controllId="formMilestone3">
+          <Form.Label column sm={2} >Milestone 3</Form.Label>
+          <Col sm={3}>
+            <Form.Control type="text" placeholder="Milestone name"></Form.Control>
+          </Col>
+          <Col sm={3}>
+            <Form.Control type="textarea" placeholder="Brief description"></Form.Control>
+          </Col>
+          <Col sm={2}>
+            <Form.Control type="decimal" placeholder="ETH"></Form.Control>
+          </Col>
+          <Col sm={2}>
+            <Form.Control type="date" placeholder="reached"></Form.Control>
+          </Col>
+        </Form.Group>
+
+        <Button variant="primary" type="submit" size="lg">
           Create Pot
         </Button>
+
       </Form>
 
     </div>
